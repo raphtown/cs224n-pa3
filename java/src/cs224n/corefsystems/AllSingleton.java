89fc6cdf0495c2c42efcaf9f6e433a8556a1e94a
Raphael Townshend <r@tc.com>
2014-11-05 15:20:38 -0800

    Add part 3.1 baselines

--- java/src/cs224n/corefsystems/AllSingleton.java
+++ java/src/cs224n/corefsystems/AllSingleton.java
@@ -1,25 +1,31 @@
 package cs224n.corefsystems;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 import cs224n.coref.ClusteredMention;
 import cs224n.coref.Document;
 import cs224n.coref.Entity;
+import cs224n.coref.Mention;
 import cs224n.util.Pair;
 
 public class AllSingleton implements CoreferenceSystem {
 
-	@Override
-	public void train(Collection<Pair<Document, List<Entity>>> trainingData) {
-		// TODO Auto-generated method stub
+    @Override
+    public void train(Collection<Pair<Document, List<Entity>>> trainingData) {
+        // Do nothing for training.
+    }
 
-	}
-
-	@Override
-	public List<ClusteredMention> runCoreference(Document doc) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public List<ClusteredMention> runCoreference(Document doc) {
+        List<ClusteredMention> mentions = new ArrayList<ClusteredMention>();
+        for(Mention m : doc.getMentions()) {
+            // New cluster for every mention.
+            ClusteredMention newCluster = m.markSingleton();
+            mentions.add(newCluster);
+        }
+        return mentions;
+    }
 
 }
--- java/src/cs224n/corefsystems/OneCluster.java
+++ java/src/cs224n/corefsystems/OneCluster.java
@@ -1,25 +1,36 @@
 package cs224n.corefsystems;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 import cs224n.coref.ClusteredMention;
 import cs224n.coref.Document;
 import cs224n.coref.Entity;
+import cs224n.coref.Mention;
 import cs224n.util.Pair;
 
 public class OneCluster implements CoreferenceSystem {
 
 	@Override
 	public void train(Collection<Pair<Document, List<Entity>>> trainingData) {
-		// TODO Auto-generated method stub
-
+	 // Do nothing for training.
 	}
 
 	@Override
 	public List<ClusteredMention> runCoreference(Document doc) {
-		// TODO Auto-generated method stub
-		return null;
+        List<ClusteredMention> mentions = new ArrayList<ClusteredMention>();
+        ClusteredMention singleCluster = null;
+        for(Mention m : doc.getMentions()) {
+            // One cluster for all mentions.
+            if (singleCluster == null) {
+                singleCluster = m.markSingleton();
+                mentions.add(singleCluster);
+            } else {
+                mentions.add(m.markCoreferent(singleCluster));
+            }
+        }
+        return mentions;
 	}
 
 }
