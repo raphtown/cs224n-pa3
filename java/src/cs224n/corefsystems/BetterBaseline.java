96e151fc5dab01352e7b4ebe7519b2e99bcffd67
Raphael Townshend <r@tc.com>
2014-11-06 01:28:24 -0800

    First pass at better baseline

--- java/src/cs224n/corefsystems/BetterBaseline.java
+++ java/src/cs224n/corefsystems/BetterBaseline.java
@@ -1,39 +1,80 @@
 package cs224n.corefsystems;
 
-import java.util.Collection;
-import java.util.List;
 import java.util.ArrayList;
-
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 
 import cs224n.coref.ClusteredMention;
 import cs224n.coref.Document;
-import cs224n.coref.*;
+import cs224n.coref.Entity;
+import cs224n.coref.Mention;
 import cs224n.util.Pair;
 
 public class BetterBaseline implements CoreferenceSystem {
+	HashMap<String, HashSet<String>> coreferentHeads = new HashMap<String, HashSet<String>>();
 
 	@Override
 	public void train(Collection<Pair<Document, List<Entity>>> trainingData) {
-		// TODO Auto-generated method stub
-
+		for(Pair<Document, List<Entity>> pair : trainingData){
+			for(Entity e : pair.getSecond()){
+				
+				// Link coreferent headwords together.
+				for(Pair<Mention, Mention> mentionPair : e.orderedMentionPairs()){
+					String referringHead = mentionPair.getFirst().headWord();
+					String referredHead = mentionPair.getSecond().headWord();
+					if (!coreferentHeads.containsKey(referringHead)) {
+						coreferentHeads.put(referringHead, new HashSet<String>());
+						coreferentHeads.get(referringHead).add(referringHead);
+					}
+					coreferentHeads.get(referringHead).add(referredHead);
+				}
+			}
+		}
 	}
 
 	@Override
 	public List<ClusteredMention> runCoreference(Document doc) {
-		// TODO Auto-generated method stub
-	
-	ArrayList<ClusteredMention> clusters = new ArrayList<ClusteredMention>();
-	for (Mention m : doc.getMentions()) {
-        if (m.gloss().equals("God the Protector")) {
-        System.out.println(m.sentence.parse);
-	System.out.println(m.parse);
-        System.out.println(m.beginIndexInclusive + " "+m.endIndexExclusive);
-        System.out.println(m.gloss());
-}
-     clusters.add(m.markSingleton());
-}
+		HashMap<String, ClusteredMention> seenHeads = new HashMap<String, ClusteredMention>();
+		ArrayList<ClusteredMention> clusters = new ArrayList<ClusteredMention>();
+		for (Mention m : doc.getMentions()) {
+			String referringHead = m.headWord();
+			boolean foundCoreferent = false;
+			
+			// Try exact matching first.
+			for (ClusteredMention seenMentions : clusters) {
+				if(m.gloss().equals(seenMentions.mention.gloss())) {
+					ClusteredMention cm = m.markCoreferent(seenMentions);
+					clusters.add(cm);
+					foundCoreferent = true;
+					break;
+				}
+			}
+			
+			// Otherwise try matching to other heads that were found to be coreferrent in the training.
+			if (!foundCoreferent && coreferentHeads.containsKey(referringHead)) {
+				for (String referredHead : coreferentHeads.get(referringHead)) {
+					if (seenHeads.containsKey(referredHead)) {
+						ClusteredMention oldCm = seenHeads.get(referredHead);
+						ClusteredMention newCm = m.markCoreferent(oldCm);
+						clusters.add(newCm);
+						seenHeads.put(referringHead, oldCm);
+						foundCoreferent = true;
+						break;
+					}
+				}
+			}
+			
+			// Otherwise, start new cluster.
+			if (!foundCoreferent) {
+				ClusteredMention cm = m.markSingleton();
+				seenHeads.put(referringHead, cm);
+				clusters.add(cm);
+			}
+		}
 
-	return clusters;
+		return clusters;
 	}
 
 }
